663a53de95cae885240417c1d9f56ed51b48160d
Margaretha Olivia <margarethaolivia41@gmail.com>
2022-11-19 09:20:09 +0700

    move algorithm to start process button

--- src/window.py
+++ src/window.py
@@ -11,10 +11,6 @@ from PIL import ImageTk, Image
 from tkinter import filedialog
 
 
-def btn_clicked():
-    print("Button Clicked")
-
-
 window = Tk()
 
 window.geometry("1200x700")
@@ -26,6 +22,73 @@ global result, res_name, mean, e, y
 fileChosen = False
 folderChosen = False
 
+foldername = ''
+filename = ''
+
+
+def startProcess():
+    print('Start process...')
+    # global myImage
+    # global fileChosen
+    global mean, e, y, res_name
+    global filename, foldername
+    if (foldername != '' and filename != ''):
+        result, res_name = main2.batch_extractor(foldername)
+        print("RESULT")
+        print(result.shape)
+        # result = result.reshape((256, 256))
+        print("\n\n")
+        mean = eucl.mean_mat(result)
+        print("MEAN MATRIX")
+        print(mean)
+        print(mean.shape)
+        # meanFace = mean.reshape((256, 256))
+        # meanFace = np.array(meanFace, dtype=np.uint8)
+        # print(meanFace)
+        # cv.imshow('displaymywindows', meanFace)
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()
+        # normalizedImg = np.zeros((255, 255))
+        # normalizedImg = cv.normalize(
+        #     eigenFace,  normalizedImg, 0, 255, cv.NORM_MINMAX)
+
+        print("\n\n")
+
+        print("TRAINING MATRIX (2048XM)")
+        A = eucl.sub_mat(result, mean)
+        print(A)
+        print(A.shape)
+
+        print("\n\n")
+
+        C1 = eucl.covariant(A)
+        print("COVARIANT MXM")
+        print(C1)
+
+        print("\n\n")
+
+        evals, eigh = eig.qr_iteration(C1, min(10, len(C1) // 10))
+        # evals = eigen_value(C1)
+        # eigh = eigen_vector(A, evals, C1)
+        v, w = np.linalg.eig(C1)
+        # print("OWNED LIB vs NUMPY")
+        print(v)
+        print(evals)
+        print("===========")
+        print("\n\n")
+        print(eigh[1])
+        print("============")
+        # np.set_printoptions(threshold=sys.maxsize)
+        print(w[1])
+        e = eig.eigen_vector(A, eigh)
+        # print(e)
+
+        y = eig.proj(e, A)
+
+        path = main2.test_image(mean, e, y, res_name, filename)
+
+        openClosestResult(path)
+
 
 def openFolder():
     global myFolder
@@ -35,67 +98,24 @@ def openFolder():
     window.foldername = filedialog.askdirectory()
     foldername = window.foldername.split(
         '/')[len(window.foldername.split('/'))-1]
-    canvas.itemconfig(
-        cfo,
-        text=foldername
-    )
-    result, res_name = main2.batch_extractor(foldername)
-    print("RESULT")
-    print(result.shape)
-    # result = result.reshape((256, 256))
-    print("\n\n")
-    mean = eucl.mean_mat(result)
-    print("MEAN MATRIX")
-    print(mean)
-    print(mean.shape)
-    # meanFace = mean.reshape((256, 256))
-    # meanFace = np.array(meanFace, dtype=np.uint8)
-    # print(meanFace)
-    # cv.imshow('displaymywindows', meanFace)
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
-    # normalizedImg = np.zeros((255, 255))
-    # normalizedImg = cv.normalize(
-    #     eigenFace,  normalizedImg, 0, 255, cv.NORM_MINMAX)
 
-    print("\n\n")
-
-    print("TRAINING MATRIX (2048XM)")
-    A = eucl.sub_mat(result, mean)
-    print(A)
-    print(A.shape)
-
-    print("\n\n")
-
-    C1 = eucl.covariant(A)
-    print("COVARIANT MXM")
-    print(C1)
-
-    print("\n\n")
-
-    evals, eigh = eig.qr_iteration(C1, min(10, len(C1) // 10))
-    # evals = eigen_value(C1)
-    # eigh = eigen_vector(A, evals, C1)
-    v, w = np.linalg.eig(C1)
-    # print("OWNED LIB vs NUMPY")
-    print(v)
-    print(evals)
-    print("===========")
-    print("\n\n")
-    print(eigh[1])
-    print("============")
-    # np.set_printoptions(threshold=sys.maxsize)
-    print(w[1])
-    e = eig.eigen_vector(A, eigh)
-    # print(e)
-
-    y = eig.proj(e, A)
+    if (foldername != ''):
+        canvas.itemconfig(
+            cfo,
+            text=foldername
+        )
+    # else:
+    #     canvas.itemconfig(
+    #         cfo,
+    #         text="No Folder Chosen"
+    #     )
 
 
 def openFile():
     global myImage
     global fileChosen
     global mean, e, y, res_name
+    global filename
     window.filename = filedialog.askopenfilename(
         initialdir="ALGEO02-21063", title="Select an image", filetypes=(("JPG Files", "*.jpg"), ("PNG Files", "*.png"), ("All Files", "*.*")))
     filename = window.filename.split('/')[len(window.filename.split('/'))-1]
@@ -104,7 +124,7 @@ def openFile():
         text=filename
     )
 
-    path = main2.test_image(mean, e, y, res_name, filename)
+    # path = main2.test_image(mean, e, y, res_name, filename)
 
     img = Image.open(window.filename)
     myImage = ImageTk.PhotoImage(img)
@@ -122,7 +142,7 @@ def openFile():
         675, 360,
         image=myImage)
 
-    openClosestResult(path)
+    # openClosestResult(path)
 
 
 def openClosestResult(path):
@@ -194,7 +214,7 @@ b2 = Button(
     image=img2,
     borderwidth=0,
     highlightthickness=0,
-    command=btn_clicked,
+    command=startProcess,
     relief="flat")
 
 b2.place(
